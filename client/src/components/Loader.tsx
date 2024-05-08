26944ba4a98b227b3bf7538db3a134e1cd9d39d4
idrissou <idrissoufaysal93@gmail.com>
2024-05-08 23:25:19 +0100

    s

---
+++ client/src/components/Loader.tsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react'
+
+// const loaderStyles = {
+//     position: "fixed",
+//     top: "50%",
+//     left: "50%",
+//     transform: "translate(-50%, -50%)",
+//   };
+
+export default function Loader() {
+  return (
+    <div>
+      {/* <div style={loaderStyles}>
+            <SyncLoader color="#4890a6" loading={true} size={15} />
+          </div> */}
+    </div>
+  )
+}
