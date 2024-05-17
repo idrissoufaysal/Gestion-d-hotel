802410ee2930e1db2dfc88758dfd34404727ac32
idrissou <idrissoufaysal93@gmail.com>
2024-05-17 18:28:50 +0100

    a

--- client/src/components/SearchItem.tsx
+++ client/src/components/SearchItem.tsx
@@ -2,8 +2,8 @@ export default function SearchItem() {
   return (
     <div className="searchItem">
       <img src="" alt="" />
-      <div className="sDesc"></div>
-      <div className="sDetails"></div>
+      <div className="sDesc">l</div>
+      <div className="sDetails">l</div>
     </div>
   );
 }
