964a88aa5f90b7aaf2e36b9479ccf83f232edf8e
idrissou <idrissoufaysal93@gmail.com>
2024-05-21 02:17:41 +0100

    s

--- client/src/components/SearchItem.tsx
+++ client/src/components/SearchItem.tsx
@@ -5,23 +5,28 @@ export default function SearchItem() {
     <div className="searchItem">
       <img src={img} alt="" />
       <div className="siDesc">
-        
-          <h1 className="siTitle">Tower Street Apartments</h1>
-          <span className="siDistance">500m from center</span>
-          <span className="siTaxi0p">Free airport taxi</span>
-          <span className="siSubtitle">
-            Studio Apartment with Air conditioning
-          </span>
-          <span className="siFeatures">
-            Entire studio 1 bathroom 21m² 1 full bed
-          </span>
-          <span className="siCancelop">Free cancellation </span>
-          <span className="siCancelOpSubtitle">
-            You can cancel later, so lock in this great price today!
-          </span>
+        <h1 className="siTitle">Tower Street Apartments</h1>
+        <span className="siDistance">500m from center</span>
+        <span className="siTaxi0p">Free airport taxi</span>
+        <span className="siSubtitle">
+          Studio Apartment with Air conditioning
+        </span>
+        <span className="siFeatures">
+          Entire studio 1 bathroom 21m² 1 full bed
+        </span>
+        <span className="siCancelop">Free cancellation </span>
+        <span className="siCancelOpSubtitle">
+          You can cancel later, so lock in this great price today!
+        </span>
       </div>
       <div className="siDetails">
-        details
+        <div className="siRating">
+          <span>Exelent</span>
+          <button>8.9</button>
+        </div>
+        <div className="siDetailTexts">
+          
+        </div>
       </div>
     </div>
   );
