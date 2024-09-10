4da2cb3623d4086eb27f281bb53dd403d44d54ff
idrissou <idrissoufaysal93@gmail.com>
2024-09-10 16:58:41 +0000

    ok

--- client/src/pages/Hotel.tsx
+++ client/src/pages/Hotel.tsx
@@ -18,26 +18,26 @@ import { formatPhotoUrl } from "../utils/function";
 import { log } from "console";
 
 const Hotel = () => {
-  const photo = [
-    {
-      src: "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_1280.jpg",
-    },
-    {
-      src: "https://cdn.pixabay.com/photo/2014/08/11/21/39/wall-416060_640.jpg",
-    },
-    {
-      src: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376_640.jpg",
-    },
-    {
-      src: "https://cdn.pixabay.com/photo/2018/01/26/08/15/dining-room-3108037_640.jpg",
-    },
-    {
-      src: "https://cdn.pixabay.com/photo/2021/10/06/15/05/bathroom-6686057_640.jpg",
-    },
-    {
-      src: "https://cdn.pixabay.com/photo/2024/05/20/10/34/ai-generated-8774721_640.jpg",
-    },
-  ];
+  // const photo = [
+  //   {
+  //     src: "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_1280.jpg",
+  //   },
+  //   {
+  //     src: "https://cdn.pixabay.com/photo/2014/08/11/21/39/wall-416060_640.jpg",
+  //   },
+  //   {
+  //     src: "https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376_640.jpg",
+  //   },
+  //   {
+  //     src: "https://cdn.pixabay.com/photo/2018/01/26/08/15/dining-room-3108037_640.jpg",
+  //   },
+  //   {
+  //     src: "https://cdn.pixabay.com/photo/2021/10/06/15/05/bathroom-6686057_640.jpg",
+  //   },
+  //   {
+  //     src: "https://cdn.pixabay.com/photo/2024/05/20/10/34/ai-generated-8774721_640.jpg",
+  //   },
+  // ];
 
   const location = useLocation();
   const hotelId = location.pathname.split("/")[2];
